a2272d15de101cf4cac3edf09315e5fcd75056c4
Jason Lloyd <jason.lloyddev@gmail.com>
2017-03-04 16:12:58 +0000

    Add express files and angular2 files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Zero Waste website
+Zero waste is a environmental movement around reducing the amount of waste you produce to nearly zero. More can be read here(http://www.grrn.org/page/what-zero-waste)
+
+## Project Technical Details
+- Front-end:
+  - AngularJS 2
+  - HTML
+  - JavaScript
+  - Bootstrap v4 for CSS
+- Back-end
+  - NodeJS
+  - MongoDB
---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
