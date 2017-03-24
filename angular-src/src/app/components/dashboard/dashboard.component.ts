beceab4ffe44b7bb28f592b34ba4311955ac296a
Jason Lloyd <jason.lloyddev@gmail.com>
2017-03-24 09:19:10 +0000

    Add functionality to delete stores on dashboard page

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { Store } from '../../interfaces/store';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  allStores: Store[] = [];
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private backendService: BackendService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.backendService.getAllStores().subscribe(stores => {
+      this.allStores = stores;
+    });
+  }
+
+  deleteStore(storeId, i){
+    console.log(storeId);
+    console.log(i);
+    this.backendService.deleteStore(storeId).subscribe(data => {
+      if(data.success){
+        this.flashMessage.show('Store deleted', {cssClass:'alert-success', timeout: 3000});
+        this.router.navigate(['/dashboard']);
+      }
+      else{
+        this.flashMessage.show('Deleting store failed', {cssClass:'alert-danger', timeout: 3000});
+      }
+    });
   }
 
 }
