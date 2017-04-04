d7344c67ac268d5afcb8a969e852399ef1264d31
Jason Lloyd <jason.lloyddev@gmail.com>
2017-04-04 22:55:15 +0100

    add back in localhost for local testing

--- angular-src/src/app/services/backend.service.ts
+++ angular-src/src/app/services/backend.service.ts
@@ -5,58 +5,59 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class BackendService {
   headers: Headers = new Headers();
+  serverHostname = "http://localhost:3000"
 
   constructor(private http:Http) {
     this.headers.append('Content-Type','application/json');
   }
 
   searchStores(query){
-    return this.http.get('/api/stores/search/' + query, {headers: this.headers})
+    return this.http.get(this.serverHostname +'/api/stores/search/' + query, {headers: this.headers})
       .map(res => res.json());
   }
 
   addStore(store){
-    return this.http.post('/api/stores/addStore', store, {headers: this.headers})
+    return this.http.post(this.serverHostname +'/api/stores/addStore', store, {headers: this.headers})
       .map(res => res.json());
   }
 
   editStore(store){
-    return this.http.put('/api/stores/'+store.id, store, {headers: this.headers})
+    return this.http.put(this.serverHostname +'/api/stores/'+store.id, store, {headers: this.headers})
       .map(res => res.json());
   }
 
   deleteStore(storeID){
-    return this.http.delete('/api/stores/'+storeID, {headers: this.headers})
+    return this.http.delete(this.serverHostname +'/api/stores/'+storeID, {headers: this.headers})
       .map(res => res.json());
   }
 
   getStore(storeID){
-    return this.http.get('/api/stores/'+storeID, {headers: this.headers})
+    return this.http.get(this.serverHostname +'/api/stores/'+storeID, {headers: this.headers})
       .map(res => res.json());
   }
 
   getAllStores(){
-    return this.http.get('/api/stores/all', {headers: this.headers})
+    return this.http.get(this.serverHostname + '/api/stores/all', {headers: this.headers})
       .map(res => res.json());
   }
 
   getAllMarkers(){
-    return this.http.get('/api/stores/locations', {headers: this.headers})
+    return this.http.get(this.serverHostname +'/api/stores/locations', {headers: this.headers})
       .map(res => res.json())
   }
 
   getMessages(){
-    return this.http.get('/message/messages', {headers: this.headers})
+    return this.http.get(this.serverHostname +'/message/messages', {headers: this.headers})
       .map(res => res.json());
   }
 
   addMessage(message){
-    return this.http.post('/message/addMessage', message, {headers: this.headers})
+    return this.http.post(this.serverHostname+'/message/addMessage', message, {headers: this.headers})
       .map(res => res.json());
   }
 
   deleteMessage(messageID){
-    return this.http.delete('/message/'+messageID, {headers: this.headers})
+    return this.http.delete(this.serverHostname+'/message/'+messageID, {headers: this.headers})
       .map(res => res.json());
   }
 }
