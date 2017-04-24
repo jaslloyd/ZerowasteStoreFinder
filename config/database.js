60e0a910036abd2e77e3f55de7c0168d2fb5bc17
Jason Lloyd <jason.lloyddev@gmail.com>
2017-04-24 22:19:01 +0100

    Add config folder instead of just databse file

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'database': 'mongodb://[username]:[password]@ds119250.mlab.com:19250/heroku_kn42m0p2',
+  'database': 'mongodb://dbadmin:passw0rd@ds119250.mlab.com:19250/heroku_kn42m0p2',
   'secret': 'mysupersecrettoken'
 };
