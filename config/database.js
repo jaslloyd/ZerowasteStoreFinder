671dd488656b3d4440e02a84ed5d4b4290d0fabd
Jason Lloyd <jason.lloyddev@gmail.com>
2017-04-24 21:53:33 +0100

    Remove temporary username and password and change default port to 80

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-  //'database': 'mongodb://localhost:27017/zerowaste',
-  'database': 'mongodb://dbadmin:passw0rd@ds119250.mlab.com:19250/heroku_kn42m0p2',
+  'database': 'mongodb://[username]:[password]@ds119250.mlab.com:19250/heroku_kn42m0p2',
   'secret': 'mysupersecrettoken'
 };
