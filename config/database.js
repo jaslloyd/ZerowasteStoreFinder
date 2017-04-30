62319b6de9836f7c0778c0bb6df3cccd6fd2fb96
Jason Lloyd <jason.lloyddev@gmail.com>
2017-04-30 22:24:26 +0100

    Improve admin dashboard look

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'database': 'mongodb://[username]:[password]@ds119250.mlab.com:19250/heroku_kn42m0p2',
+  'database': 'mongodb://dbadmin_secure:H8s60kKYR4D745yY8RgvOKctC4f5brMYexR8C44P@ds119250.mlab.com:19250/heroku_kn42m0p2',
   'secret': 'mysupersecrettoken'
 };
