d55421a74648e07cd2d9c8333293ea03cb63ab9e
Jason Lloyd <jason.lloyddev@gmail.com>
2017-05-27 19:52:05 +0100

    Update paths in the deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@ echo "Starting deployment..."
 pm2 stop www
 rm -rf /home/jason/ZerowasteStoreFinder/
 cd /home/jason/;git clone git@github.com:TheDeployGuy/ZerowasteStoreFinder.git
-cp /tmp/database.js /home/jason/ZerowasteStoreFinder/config/database.js
+mkdir /home/jason/ZerowasteStoreFinder/config
+cp database.js /home/jason/ZerowasteStoreFinder/config/database.js
 cd /home/jason/ZerowasteStoreFinder/;npm install
 pm2 start /home/jason/ZerowasteStoreFinder/bin/www
 pm2 list
