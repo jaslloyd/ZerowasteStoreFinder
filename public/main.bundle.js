4cdacb51447deeae44d99aa0507b53289cf4b0ee
Jason Lloyd <jason.lloyddev@gmail.com>
2017-11-05 01:33:22 +0000

    Build front end

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -1516,7 +1516,7 @@ var _a;
 // The file contents for the current environment will overwrite these during build.
 var environment = {
     production: false,
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://localhost:3000'
 };
 //# sourceMappingURL=environment.js.map
 
