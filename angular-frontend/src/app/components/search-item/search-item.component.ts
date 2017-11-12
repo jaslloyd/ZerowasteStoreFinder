0368846a300218d542415de286aedded265a2e27
Jason Lloyd <jason.lloyddev@gmail.com>
2017-11-12 14:12:05 +0000

    Add map to search item

--- angular-frontend/src/app/components/search-item/search-item.component.ts
+++ angular-frontend/src/app/components/search-item/search-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AgmCoreModule, MapsAPILoader } from '@agm/core';
+
 import { Store } from '../../interfaces/store';
+import { Marker } from '../../interfaces/marker';
 
 @Component({
   selector: 'app-search-item',
@@ -12,9 +15,18 @@ export class SearchItemComponent implements OnInit {
   @Input() dayIndex: number;
 
   showMoreInfo: boolean = false;
+  zoom: number = 15;
+  marker: Marker;
+  
+  constructor(private loader: MapsAPILoader) { }
 
-  constructor() { }
-
-  ngOnInit() { }
+  ngOnInit() {
+    this.marker = {
+      lat: this.store.lat,
+      lng: this.store.lng,
+      label: this.store.name,
+      draggable: false
+    }
+  }
 
 }
