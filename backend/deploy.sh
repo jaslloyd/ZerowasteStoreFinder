3e649fe1e54714d8b59fc43b53ce3c2f786e1072
Jason Lloyd <jason.lloyddev@gmail.com>
2018-05-02 18:30:18 +0100

    Update deployment script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,9 +2,9 @@ echo "Starting deployment..."
 pm2 stop www
 rm -rf /home/jason/ZerowasteStoreFinder/
 cd /home/jason/;git clone git@github.com:TheDeployGuy/ZerowasteStoreFinder.git
-mkdir /home/jason/ZerowasteStoreFinder/config
-cp database.js /home/jason/ZerowasteStoreFinder/config/database.js
-cd /home/jason/ZerowasteStoreFinder/;npm install
-pm2 start /home/jason/ZerowasteStoreFinder/bin/www
+mkdir /home/jason/ZerowasteStoreFinder/backend/config/
+cp database.js /home/jason/ZerowasteStoreFinder/backend/config/database.js
+cd /home/jason/ZerowasteStoreFinder/backend;npm install
+pm2 start /home/jason/ZerowasteStoreFinder/backend/bin/www
 pm2 list
 echo "Deployment finished"
